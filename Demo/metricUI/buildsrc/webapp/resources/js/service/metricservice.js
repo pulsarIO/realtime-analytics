4709f2f75f649f596baf6e24d79ca7f331c0c1eb
U-CORP\mingmwang <mingmwang@ebay.com>
2015-02-13 17:21:56 +0800

    fix URL

--- Demo/metricUI/buildsrc/webapp/resources/js/service/metricservice.js
+++ Demo/metricUI/buildsrc/webapp/resources/js/service/metricservice.js
@@ -1,4 +1,4 @@
 angular.module("indexApp").factory('MetricService', ['$resource', function ($resource) {
    "use strict";
-   return $resource('http://10.64.254.178:8083/pulsar/metric?:params' + '', {params:null}, {query: {method: 'GET', cache:false, isArray:true}});
+   return $resource('http://'+location.host+'/pulsar/metric?:params' + '', {params:null}, {query: {method: 'GET', cache:false, isArray:true}});
 }]);
