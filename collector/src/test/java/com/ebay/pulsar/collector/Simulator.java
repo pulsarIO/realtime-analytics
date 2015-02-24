30841ef06799fd8ff26a177c03f846aeaeb353f8
Xinglang Wang <wangxinglang@gmail.com>
2015-02-24 12:23:48 +0800

    Change License to GPLv2 only

--- collector/src/test/java/com/ebay/pulsar/collector/Simulator.java
+++ collector/src/test/java/com/ebay/pulsar/collector/Simulator.java
@@ -1,7 +1,7 @@
 /*
 Pulsar
 Copyright (C) 2013-2015 eBay Software Foundation
-Dual licensed under the Apache 2.0 license and the GPL v2 license.  See LICENSE for full terms.
+Licensed under the GPL v2 license.  See LICENSE for full terms.
 */
 package com.ebay.pulsar.collector;
 
